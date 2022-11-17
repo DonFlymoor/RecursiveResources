c0318c0e25fde1be9e13afad41fafd6ec6bfc196
Tex <314084612@qq.com>
2022-11-17 16:17:22 +0800

    port

--- src/main/java/nl/enjarai/recursiveresources/RecursiveResources.java
+++ src/main/java/nl/enjarai/recursiveresources/RecursiveResources.java
@@ -1,13 +1,26 @@
 package nl.enjarai.recursiveresources;
 
-import net.fabricmc.api.ClientModInitializer;
+//import net.fabricmc.api.ClientModInitializer;
 import net.minecraft.util.Identifier;
+import net.minecraftforge.fml.IExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.network.NetworkConstants;
 
-public class RecursiveResources implements ClientModInitializer {
+@Mod(RecursiveResources.MOD_ID)
+public class RecursiveResources {
     public static final String MOD_ID = "recursiveresources";
 
-    @Override
-    public void onInitializeClient() {
+    public RecursiveResources() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onInitializeClient);
+
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+
+    }
+
+    public void onInitializeClient(final FMLClientSetupEvent event) {
 
     }
 
